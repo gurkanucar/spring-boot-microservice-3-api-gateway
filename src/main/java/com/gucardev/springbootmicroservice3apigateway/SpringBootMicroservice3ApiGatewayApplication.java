6392f022752ceb5b7920a23932851ea6e8afcd78
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-04-24 21:23:40 +0300

    OpenFeign enabled and configuration class created

--- src/main/java/com/gucardev/springbootmicroservice3apigateway/SpringBootMicroservice3ApiGatewayApplication.java
+++ src/main/java/com/gucardev/springbootmicroservice3apigateway/SpringBootMicroservice3ApiGatewayApplication.java
@@ -2,7 +2,9 @@ package com.gucardev.springbootmicroservice3apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class SpringBootMicroservice3ApiGatewayApplication {
 
---
+++ src/main/java/com/gucardev/springbootmicroservice3apigateway/request/FeignConfiguration.java
@@ -0,0 +1,18 @@
+package com.gucardev.springbootmicroservice3apigateway.request;
+
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfiguration {
+
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor(@Value("${service.security-secure-key-username}") String username,
+                                                                   @Value("${service.security-secure-key-password}") String password) {
+        return new BasicAuthRequestInterceptor(username, password);
+    }
+
+
+}
