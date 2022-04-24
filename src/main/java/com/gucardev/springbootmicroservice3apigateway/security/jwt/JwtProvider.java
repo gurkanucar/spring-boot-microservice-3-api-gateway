eb2810d734f07469ee1cc9e1c5dbca3fd850dbc6
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-04-24 18:04:21 +0300

    jwt provider created

---
+++ src/main/java/com/gucardev/springbootmicroservice3apigateway/security/jwt/JwtProvider.java
@@ -0,0 +1,96 @@
+package com.gucardev.springbootmicroservice3apigateway.security.jwt;
+
+import com.gucardev.springbootmicroservice3apigateway.security.UserPrincipal;
+import com.gucardev.springbootmicroservice3apigateway.util.SecurityUtils;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtProvider {
+
+    @Value("${app.jwt.secret}")
+    private String JWT_SECRET;
+
+    @Value("${app.jwt.expiration-in-ms}")
+    private Long JWT_EXPIRATION_IN_MS;
+
+
+    public String generateToken(UserPrincipal auth) {
+        String authorities = auth.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+        Key key = Keys.hmacShaKeyFor(JWT_SECRET.getBytes(StandardCharsets.UTF_8));
+        return Jwts.builder()
+                .setSubject(auth.getUsername())
+                .claim("roles", authorities)
+                .claim("userId", auth.getId())
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION_IN_MS))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+
+    }
+
+    public Authentication getAuthentication(HttpServletRequest request) {
+        Claims claims = extractClaims(request);
+        if (claims == null) {
+            return null;
+        }
+        String username = claims.getSubject();
+        Long userId = claims.get("userId", Long.class);
+        Set<GrantedAuthority> authorities = Arrays.stream(claims.get("roles").toString().split(","))
+                .map(SecurityUtils::convertToAuthority)
+                .collect(Collectors.toSet());
+        UserDetails userDetails = UserPrincipal.builder()
+                .username(username)
+                .authorities(authorities)
+                .id(userId)
+                .build();
+        if (username == null) {
+            return null;
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
+
+    }
+
+
+    public boolean isTokenValid(HttpServletRequest request) {
+        Claims claims = extractClaims(request);
+        if (claims == null) {
+            return false;
+        }
+        return !claims.getExpiration().before(new Date());
+    }
+
+
+    private Claims extractClaims(HttpServletRequest request) {
+        String token = SecurityUtils.extractAuthTokenFromRequest(request);
+        if (token == null) {
+            return null;
+        }
+        Key key = Keys.hmacShaKeyFor(JWT_SECRET.getBytes(StandardCharsets.UTF_8));
+
+        var parsedJwt = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build();
+
+        return parsedJwt.parseClaimsJwt(token).getBody();
+    }
+
+}
--- src/main/java/com/gucardev/springbootmicroservice3apigateway/util/SecurityUtils.java
+++ src/main/java/com/gucardev/springbootmicroservice3apigateway/util/SecurityUtils.java
@@ -1,12 +1,18 @@
 package com.gucardev.springbootmicroservice3apigateway.util;
 
+import com.ctc.wstx.util.StringUtil;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 public class SecurityUtils {
 
     public static final String ROLE_PREFIX = "ROLE_";
+    public static final String AUTH_HEADER = "authorization";
+    public static final String AUTH_TOKEN_PREIFX = "Bearer ";
 
     public static SimpleGrantedAuthority convertToAuthority(String role) {
         String formattedRole = role.startsWith(ROLE_PREFIX) ? role : ROLE_PREFIX + role;
@@ -14,5 +20,12 @@ public class SecurityUtils {
 
     }
 
+    public static String extractAuthTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTH_HEADER);
+        if (StringUtils.hasLength(bearerToken) && bearerToken.startsWith(AUTH_TOKEN_PREIFX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
 
 }
