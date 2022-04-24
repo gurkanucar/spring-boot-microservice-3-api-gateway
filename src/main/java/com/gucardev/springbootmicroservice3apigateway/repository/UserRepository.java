33ecc365232fde270dd932b5f3f85d139fe3a272
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-04-24 17:02:08 +0300

    user repository created

---
+++ src/main/java/com/gucardev/springbootmicroservice3apigateway/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.gucardev.springbootmicroservice3apigateway.repository;
+
+import com.gucardev.springbootmicroservice3apigateway.model.Role;
+import com.gucardev.springbootmicroservice3apigateway.model.User;
+import feign.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByUsername(String username);
+
+    @Modifying
+    @Query("update User set role = :role where username = :username")
+    void updateUserRole(@Param("username") String username, @Param("role") Role role);
+
+}
