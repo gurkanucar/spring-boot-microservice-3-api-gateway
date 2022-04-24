f4c466ad2690efcf81ab2a05665b95e710fdd32d
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-04-24 17:07:26 +0300

    user service created

---
+++ src/main/java/com/gucardev/springbootmicroservice3apigateway/UserService.java
@@ -0,0 +1,45 @@
+package com.gucardev.springbootmicroservice3apigateway;
+
+import com.gucardev.springbootmicroservice3apigateway.model.Role;
+import com.gucardev.springbootmicroservice3apigateway.model.User;
+import com.gucardev.springbootmicroservice3apigateway.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public UserService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(Role.USER);
+        user.setCreated(LocalDateTime.now());
+
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Transactional //Transactional is required when executing an update/delete query.
+    public void changeRole(Role newRole, String username) {
+        userRepository.updateUserRole(username, newRole);
+    }
+
+
+}
