eb30087abff73ce9ce68e574a0e3bf166fdb1ebb
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-04-24 21:44:44 +0300

    course service feign config created

---
+++ src/main/java/com/gucardev/springbootmicroservice3apigateway/request/CourseServiceRequest.java
@@ -0,0 +1,23 @@
+package com.gucardev.springbootmicroservice3apigateway.request;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(value = "course-service", path = "/api/course", url = "http://localhost:8081", configuration = FeignConfiguration.class)
+public interface CourseServiceRequest {
+
+    @PostMapping
+        // /api/course
+    Object saveCourse(@RequestBody Object requestBody);
+
+    @DeleteMapping("{courseId}")
+        //api/course/{courseId}
+    void deleteCourse(@PathVariable("courseId") Long courseId);
+
+    @GetMapping
+        // /api/course
+    List<Object> getAllCourses();
+
+}
