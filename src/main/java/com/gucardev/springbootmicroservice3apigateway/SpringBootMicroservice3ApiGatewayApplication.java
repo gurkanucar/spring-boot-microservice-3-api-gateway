1eb058271d5e7157d2ea0da9fded7d2d2154b397
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-04-24 16:19:11 +0300

    Initial commit

---
+++ src/main/java/com/gucardev/springbootmicroservice3apigateway/SpringBootMicroservice3ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.springbootmicroservice3apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservice3ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMicroservice3ApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gucardev/springbootmicroservice3apigateway/SpringBootMicroservice3ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.springbootmicroservice3apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservice3ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
